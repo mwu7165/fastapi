1d83958522a7c59e8ad4faa776ab9ad6ab043b69
Maria Wo <mwu7165@vt.edu>
2024-08-14 12:01:08 -0400

    Second Commit

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+# main.py
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
+items = []
+
 @app.get("/")
-def root():
-    return {"Hello":"World"}
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return items
